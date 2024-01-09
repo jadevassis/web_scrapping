0de4b259a667e0f1703dd927fa0b318021b3fe63
Jader Assis <94124050+jadevassis@users.noreply.github.com>
2024-01-09 07:21:18 -0300

    Delete dio-twitter-py-master directory

--- dio-twitter-py-master/src/connection.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://dio:dio@localhost:27017/")
-
-db = client.dio_live
-
-trends_collection = db.trends
--- dio-twitter-py-master/src/responses.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class TrendItem(BaseModel):
-    name: str
-    url: str
